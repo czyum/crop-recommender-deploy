9f193b96b79dd83c9f1fac1b9410d61ba0103f34
czyum <chethan27091999@gmail.com>
2020-12-29 14:53:26 +0530

    Added meta tags

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.secret_key="."
 y_label=None
+
 Type={'rice': 0, 'wheat': 1, 'Mung Bean': 2, 'Tea': 3, 'millet': 4, 'maize': 5, 'Lentil': 6,\
      'Jute': 7, 'Coffee': 8, 'Cotton': 9, 'Ground Nut': 10, 'Peas': 11, 'Rubber': 12, \
          'Sugarcane': 13, 'Tobacco': 14, 'Kidney Beans': 15, 'Moth Beans': 16,\
@@ -14,10 +15,12 @@ Type={'rice': 0, 'wheat': 1, 'Mung Bean': 2, 'Tea': 3, 'millet': 4, 'maize': 5, 
                   'Chickpea': 21, 'banana': 22, 'grapes': 23, 'apple': 24, 'mango': 25,\
                        'muskmelon': 26, 'orange': 27, 'papaya': 28, 'pomegranate': 29, 'watermelon': 30}
 Crop={v:k for k,v in Type.items()} 
+
+# Loading the model and scaler using pickle
 model=pickle.load(open('./static/CropPredictsvm.pkl', 'rb'))
 scaler=pickle.load(open('./static/CropScaler.pkl', 'rb'))
 
-            
+# HAndling the requests made to root page   
 @app.route("/",methods=["GET","POST"])
 
 
