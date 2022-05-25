dc56e6b692b5fe1c396e829a1d555f0d5312af9f
ChethanN <chethan.n@bhnetwork.com>
2022-05-25 13:43:35 +0530

    initial commit knn

--- app.py
+++ app.py
@@ -8,17 +8,9 @@ app = Flask(__name__)
 app.secret_key="."
 y_label=None
 
-Type={'rice': 0, 'wheat': 1, 'Mung Bean': 2, 'Tea': 3, 'millet': 4, 'maize': 5, 'Lentil': 6,\
-     'Jute': 7, 'Coffee': 8, 'Cotton': 9, 'Ground Nut': 10, 'Peas': 11, 'Rubber': 12, \
-         'Sugarcane': 13, 'Tobacco': 14, 'Kidney Beans': 15, 'Moth Beans': 16,\
-             'Coconut': 17, 'Black gram': 18, 'Adzuki Beans': 19, 'Pigeon Peas': 20,\
-                  'Chickpea': 21, 'banana': 22, 'grapes': 23, 'apple': 24, 'mango': 25,\
-                       'muskmelon': 26, 'orange': 27, 'papaya': 28, 'pomegranate': 29, 'watermelon': 30}
-Crop={v:k for k,v in Type.items()} 
 
 # Loading the model and scaler using pickle
-model=pickle.load(open('./static/CropPredictsvm.pkl', 'rb'))
-scaler=pickle.load(open('./static/CropScaler.pkl', 'rb'))
+model=pickle.load(open('./static/knn.pickle', 'rb'))
 
 # HAndling the requests made to root page   
 @app.route("/",methods=["GET","POST"])
@@ -26,18 +18,18 @@ scaler=pickle.load(open('./static/CropScaler.pkl', 'rb'))
 
 def index():
     if request.method=="POST":
-        temperature=request.form['temperature']
-        humidity=request.form['humidity']
-        ph=request.form['ph']
-        rainfall=request.form['rainfall']
-        data={'temperature':[temperature],'humidity':[humidity],'ph':[ph],'rainfall':[rainfall],}
+        petal_length=request.form['petal_length']
+        petal_width=request.form['petal_width']
+        sepal_length=request.form['sepal_length']
+        sepal_width=request.form['sepal_width']
+       
+        data={'PetalLengthCm':[petal_length],'PetalWidthCm':[petal_width],'SepalLengthCm':[sepal_length],'SepalWidthCm':[sepal_width]}
         df=pd.DataFrame(data=data)
-        scaled_data=scaler.transform(df)
-        y_label=model.predict(scaled_data)
-        prediction=Crop[y_label[0]].upper()
+        y_label=model.predict(df)
+        prediction=y_label[0].upper()
         
         return render_template("prediction.html",\
-            temperature=temperature,humidity=humidity,ph=ph,rainfall=rainfall,prediction=prediction)
+            petal_length=petal_length,petal_width=petal_width,sepal_length=sepal_length,sepal_width=sepal_width,prediction=prediction)
         
 
     return render_template("index.html")
